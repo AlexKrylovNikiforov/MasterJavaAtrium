e8b02bc7b1c0270be55ef9c1af407622939f2056
AlexKrylovNikiforov <gomezkorta@gmail.com>
2025-03-12 19:08:44 +0000

    ejercicios anadidios

---
+++ MasterGrupoAtrium/src/ejercicios_propuestos_estructuras_iterativas_for/OddFinder.java
@@ -0,0 +1,15 @@
+package ejercicios_propuestos_estructuras_iterativas_for;
+
+public class OddFinder {
+	
+
+	public static void main(String[] args) {
+		
+		for(int i = 30; i > 0; i--) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
